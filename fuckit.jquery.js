b8234d7cd607fae826532af689fd82d06e5fd698
Matt Diamond <mdiamond@onerecovery.com>
2012-06-18 17:11:00 -0700

    ADDING JQUERY VERSION FOR JQUERY

---
+++ fuckit.jquery.js
@@ -0,0 +1,14 @@
+/*
+    FuckItJS v1.0.0-alpha
+    Copyright 2012, Matt Diamond
+
+    Note: This is ALPHA software and may result in irreversible brain damage.
+ */
+
+$(function(){
+  try {
+    // YOUR CODE GOES HERE
+  } catch(e){
+    // fuck it.
+  }
+});

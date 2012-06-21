2102783dfd47683d3a96eebb37651d21678c67d9
Matt Diamond <mdiamond@onerecovery.com>
2012-06-21 16:38:08 -0700

    MAJOR UPGRADE, MIND BLOWN

--- fuckit.js
+++ fuckit.js
@@ -1,12 +1,44 @@
 /*
-    FuckItJS v1.0.0-alpha
+    FuckItJS v2.0.0-alpha
     Copyright 2012, Matt Diamond
 
     Note: This is ALPHA software and may result in irreversible brain damage.
  */
 
-try {
-  // YOUR CODE GOES HERE
-} catch(e){
-  // fuck it.
-}
+(function($){
+
+  var _FuckIt = window.FuckIt;
+
+  var FuckIt = function(script){
+    if (window.fuckedScript){
+      eval(window.fuckedScript);
+      return;
+    }
+    var req = $.ajax({
+      url: script,
+      dataType: "text"
+    });
+    req.always(function(result){
+      window.fuckedScript = result;
+      eval(fuckedScript);
+    });
+  }
+
+  window.onerror = function(error, url, line){
+    var parsed = window.fuckedScript.split("\n");
+    parsed.splice(line - 1, 1);
+    window.fuckedScript = parsed.join("\n");
+    $.getScript("fuckit.js", function(){
+      eval(window.fuckedScript);
+    });
+    return true;
+  }
+
+  FuckIt.noConflict = function(){
+    window.FuckIt = _FuckIt;
+    return FuckIt;
+  }
+
+  window.FuckIt = FuckIt;
+
+})(jQuery);
---
+++ test.js
@@ -0,0 +1,9 @@
+var x = fuuuuu
+var y = 2;
+
+buh
+
+alert(x);
+  heh heh
+
+alert(y);
